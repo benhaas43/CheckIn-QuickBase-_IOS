b78875a07a91206e8d440a9d5d1fdebecb88b980
Tsiganenko <tsiganenkoa@gmail.com>
2015-08-25 15:47:36 +0800

    Initial Commit

---
+++ CheckIn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckIn
+//
+//  Created by heliumsoft on 8/25/15.
+//  Copyright (c) 2015 Glenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheckIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckIn
+//
+//  Created by heliumsoft on 8/25/15.
+//  Copyright (c) 2015 Glenn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
